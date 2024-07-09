62b8a9329884a98931632e79c8949e4e3f81f3fa
Gökhan Perçem <35665392+gokhanproje@users.noreply.github.com>
2024-07-10 00:42:22 +0300

    initialize project

---
+++ App/App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import { SafeAreaView, StyleSheet } from "react-native";
+
+
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView style={styles.backgroundStyle}>
+
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+
+  },
+
+});
+
+export default App;
